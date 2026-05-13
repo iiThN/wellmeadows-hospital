095fee2e236911f1dcb2637e88fd38d1fc033c1d
Anthony Capuras <capuras.anthony2003@gmail.com>
2026-05-13 14:28:20 +0800

    fix missing url import for https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,24 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;   // ← this line is missing!
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-         if (config('app.env') === 'production') {
-             URL::forceScheme('https');  // add this
-         }
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
